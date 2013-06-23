459580e1504c338e69f7c618124b45433e940d10
Qasim Zaidi <qasim@zaidi.me>
2013-06-23 10:50:18 +0530

    allow Cross domain requests

--- templates/project/server.js
+++ templates/project/server.js
@@ -3,6 +3,9 @@ var dashing = require('dashing-js').Dashing();
 // Set your auth token here
 //dashing.auth_token = 'YOUR_AUTH_TOKEN';
 
+// Set cors allowed domains here
+//dashing.allowedOrigins = 'https://www.google.com';
+
 /*
 dashing._protected = function(req, res, next) {
   // Put any authentication code you want in here.
